ece48a25f1428d97dfe49232736e3c769b3ca9e7
Aidan Payne <Terlen@users.noreply.github.com>
2017-08-28 23:17:11 -0700

    Added download support
    Downloads fetched images to current directory

--- rssTest.py
+++ rssTest.py
@@ -1,10 +1,14 @@
 import re
 import feedparser
+import urllib.request
 awwPics = feedparser.parse('https://www.reddit.com/r/aww/top/.rss?sort=top&t=week')
 for x in range(0,len(awwPics.entries)):
     awwPicsImage = re.search('https:\/\/i\.redd\.it\/.*\.jpg', awwPics.entries[x].content[0].value)
     try:
         print(awwPicsImage.group(0))
+        file = awwPicsImage.group(0).rsplit('/')[-1]
+        print(file)
+        urllib.request.urlretrieve(awwPicsImage.group(0), file)
     except AttributeError:
         continue
 
